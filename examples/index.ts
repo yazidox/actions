2fcab0b32dd7b1fee492a25f84326ae777f845c0
KhepriDev <yazidkherrati@gmail.com>
2024-06-26 02:06:19 +0100

    init

--- examples/index.ts
+++ examples/index.ts
@@ -4,13 +4,29 @@ import jupiterSwap from './jupiter-swap/route';
 import { cors } from 'hono/cors';
 import { swaggerUI } from '@hono/swagger-ui';
 import { OpenAPIHono } from '@hono/zod-openapi';
+import { serveStatic } from 'hono/serve-static';
 
 const app = new OpenAPIHono();
 app.use('/*', cors());
 
+// Serve static files from the 'public' directory
+app.use('/public/*', serveStatic({
+  root: './public',
+  getContent: async (path, c) => {
+    // Implement your logic to fetch the content
+    // For example, you can use fs.promises.readFile to read the file
+    const fs = require('fs').promises;
+    try {
+      const data = await fs.readFile(path);
+      return new Response(data);
+    } catch (error) {
+      return null;
+    }
+  }
+}));
+
 // <--Actions-->
-app.route('/api/donate', donate);
-app.route('/api/jupiter/swap', jupiterSwap);
+app.route('/', donate);
 // </--Actions-->
 
 app.doc('/doc', {
@@ -28,7 +44,7 @@ app.get(
   }),
 );
 
-const port = 3000;
+const port =  Number(process.env.PORT) || 3000;
 console.log(
   `Server is running on port ${port}
 Visit http://localhost:${port}/swagger-ui to explore existing actions
